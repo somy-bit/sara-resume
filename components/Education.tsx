a3d29aa6660e3347240105bc9e53d4c9efe6981f
Sara <sara@Saras-MacBook-Pro.local>
2025-06-27 18:04:12 -0700

    fix

--- components/Education.tsx
+++ components/Education.tsx
@@ -7,11 +7,11 @@ function Education() {
         <span className='font-semibold text-sm'> Bacholar's degree of software engineering</span> from Payame Noor University(pnu) From Iran,Tehran
       </p>
       <p className='text-gray-800 text-sm font-light'>
-        Start at <span className='font-semibold text-gray-700'>2011-09</span>
+        Start at <span className='font-semibold text-gray-700'>2011</span>
         
       </p>
         <p className='text-gray-800 text-sm font-light'>
-        End at <span className='font-semibold text-gray-700'>2015</span> 
+        End at <span className='font-semibold text-gray-700'>2014</span> 
       </p>
     </div>
   )
